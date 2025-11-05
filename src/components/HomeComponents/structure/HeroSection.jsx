659087b8534ba385ed18930a7776720b049df936
AZEEM KHAN <azeemkh778@gmail.com>
2025-11-05 17:14:08 +0500

    fixed

--- src/components/HomeComponents/structure/HeroSection.jsx
+++ src/components/HomeComponents/structure/HeroSection.jsx
@@ -13,7 +13,7 @@ const HeroSection = () => {
         <Flex vertical gap={20} justify='center' align='center' className='main-flex'>
           <Flex vertical gap={20} className='w-700' align='center'>
             <Text className='custom-text'>{t('Qloop – Powering Appointments for Clinics, Spas, Salons & General Businesses')}</Text>
-            <Title level={1} className='m-0 text-center main-heading'>{t('Smart WhatsApp Booking for')}<span className='text-brand'>{t('Modern Businesses')}</span></Title>
+            <Title level={1} className='m-0 text-center main-heading'>{t('Smart WhatsApp Booking for')} <span className='text-brand'>{t('Modern Businesses')}</span></Title>
             <Text className='fs-16'>{t('Manage bookings, reduce no-shows, and engage clients — all from one powerful platform integrated with WhatsApp.')}</Text>
             <Button className='btn bg-brand text-white' onClick={()=>navigate('/bookdemo')}>{t('Book A Demo')}</Button>
           </Flex>
